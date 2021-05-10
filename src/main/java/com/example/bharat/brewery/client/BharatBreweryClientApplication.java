9864c871d86383c109098952a38c1024b0a1a94f
Bharat Bisht <bharatbisht071139ece@gmail.com>
2021-05-10 20:42:26 +0530

    Initial branch

---
+++ src/main/java/com/example/bharat/brewery/client/BharatBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.bharat.brewery.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BharatBreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BharatBreweryClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bharat/brewery/client/BharatBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bharat.brewery.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BharatBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
